a9133714e03617ed1152c8757b1b59f318527c02
Taufik Pragusga <taufik@pragusga.com>
2023-08-11 18:59:00 +0700

    perf: use millionjs to increse the performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [million.vite({ auto: true }),tsconfigPaths(), react()],
 });
