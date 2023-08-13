94a140d5062997340982e6ae9d5d673886a98a66
Taufik Pragusga <taufik@pragusga.com>
2023-08-13 11:46:39 +0700

    feat: set staleTime to 5 seconds for fetching user status

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -5,6 +5,7 @@ import { parseUserData } from '@/utils';
 export const useUserQuery = (username: string) => {
   const query = useQuery(['user', username], {
     queryFn: () => getUser(username).then(parseUserData),
+    staleTime: 5000, // 5 seconds
   });
 
   return query;
