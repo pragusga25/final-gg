09ec228389073c6308a43df1d4b1e537e07722c1
Taufik Pragusga <taufik@pragusga.com>
2023-08-13 21:08:41 +0700

    feat: add text in PersistLoginLoader

--- src/components/PersistLogin/index.tsx
+++ src/components/PersistLogin/index.tsx
@@ -1,23 +1,15 @@
 import { usePersistLogin } from '@/hooks';
 import { FC, ReactNode } from 'react';
+import { PersistLoginLoader } from './PersistLoginLoader';
 
 type PersistLoginProps = {
   children: ReactNode;
 };
 
 export const PersistLogin: FC<PersistLoginProps> = ({ children }) => {
-  const { isLoading } = usePersistLogin();
-
-  if (isLoading)
-    return (
-      <div>
-        <div className="flex flex-col items-center justify-center h-screen">
-          <div className="flex items-center justify-center">
-            <div className="loading loading-ring loading-lg text-teal-400"></div>
-          </div>
-        </div>
-      </div>
-    );
+  let { isLoading } = usePersistLogin();
+  isLoading = true;
+  if (isLoading) return <PersistLoginLoader />;
 
   return children;
 };
