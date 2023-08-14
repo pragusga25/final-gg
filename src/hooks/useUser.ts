d6fc3ad0dff068320af2791fb1d450aa44ae2026
Taufik Pragusga <taufik@pragusga.com>
2023-08-14 07:51:31 +0700

    fix: fix edit button wont show

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,10 +1,15 @@
 import { getUser } from '@/api';
 import { useQuery } from '@tanstack/react-query';
 import { parseUserData } from '@/utils';
+import { useAuth } from '@/hooks';
 
 export const useUserQuery = (username: string) => {
+  const {
+    auth: { user },
+  } = useAuth();
   const query = useQuery(['user', username], {
-    queryFn: () => getUser(username).then(parseUserData),
+    queryFn: () =>
+      getUser(username).then((data) => parseUserData(data, user?.id)),
     staleTime: 5000, // 5 seconds
   });
 
